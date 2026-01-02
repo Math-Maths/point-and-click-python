01f75b793a8d04f76ce5f72c8ec723da955efdf5
Matheus <matheus_godoy97@hotmail.com>
2026-01-02 15:49:12 -0300

    feature: implemented the player movement, animation and cursor mouse follows

--- point-and-click-python.py
+++ point-and-click-python.py
@@ -1,15 +1,57 @@
-from pgzhelper import Actor
+from pgzhelper import Actor, keyboard
 import pgzrun
 
-# Screen resolution (change as needed)
+#---------------- CONSTANTS ----------------#
+# Game resolution settings
 WIDTH = 512
 HEIGHT = 288
-
 TILE_SIZE = 16
+
+# Map layers
 background_tiles = []
 scenario_tiles = []
 object_tiles = []
 
+#---------------- ENTITIES ----------------#
+# Player Creation
+player = Actor("player/tile_0000")
+player.pos = (WIDTH // 2, HEIGHT // 2)
+player.speed = 2
+player.facing_right = True
+player.anim_timer = 0
+player.frame = 0
+
+player_right_walk_frames = [
+    "player/tile_0000",
+    "player/tile_0001",
+]
+
+player_left_walk_frames = [
+    "player/tile_0004",
+    "player/tile_0005",
+]
+
+player_right_idle_frames = [ 
+    "player/tile_0000",
+    "player/tile_0007",
+    "player/tile_0000",
+    "player/tile_0008"
+]
+
+player_left_idle_frames = [
+    "player/tile_0004",
+    "player/tile_0006",
+    "player/tile_0004",
+    "player/tile_0009"
+]
+# END - Player creation
+
+# Cursor creation
+cursor = Actor("cursor/tile_0025")
+cursor.pos = (WIDTH // 2, HEIGHT // 2)
+# END - Cursor creation
+
+#---------------- FUNCTIONS ----------------#
 def load_level(filename):
     tiles = []
 
@@ -33,13 +75,77 @@ def load_level(filename):
             tiles.append(tile)
 
     return tiles
-    
 
-# 🔹 Load level on startup
+
+#---------------- LEVEL LOADING ------------------#
 background_tiles = load_level("level_1_background")
 scenario_tiles = load_level("level_1_scenario")
 object_tiles = load_level("level_1_objects")
 
+#---------------- UPDATE -------------------------#
+def update():
+    # Player movement and animation
+    move_x = 0
+    move_y = 0
+    moving = False
+
+    if keyboard.a:
+        move_x -= player.speed
+        #player.facing_right = False
+        moving = True
+
+    if keyboard.d:
+        move_x += player.speed
+        #player.facing_right = True
+        moving = True
+
+    if keyboard.w:
+        move_y -= player.speed
+        moving = True
+
+    if keyboard.s:
+        move_y += player.speed
+        moving = True
+
+    player.x += move_x
+    player.y += move_y
+
+    if moving:
+        player.anim_timer += 1
+        if player.anim_timer >= 10:
+            player.anim_timer = 0
+            if player.facing_right:
+                player.frame = (player.frame + 1) % len(player_right_walk_frames)
+                player.image = player_right_walk_frames[player.frame]
+            else:
+                player.frame = (player.frame + 1) % len(player_left_walk_frames)
+                player.image = player_left_walk_frames[player.frame]
+    else:
+        player.anim_timer += 1
+        if player.anim_timer >= 15:
+            player.anim_timer = 0
+            if player.facing_right:
+                player.frame = (player.frame + 1) % len(player_right_idle_frames)
+                player.image = player_right_idle_frames[player.frame]
+            else:
+                player.frame = (player.frame + 1) % len(player_left_idle_frames)
+                player.image = player_left_idle_frames[player.frame]
+    #END - Player movement and animation
+
+def on_mouse_move(pos):
+    player_position = player.pos
+    mouse_x, mouse_y = pos
+
+    cursor.pos = pos
+
+    if mouse_x < player_position[0]:
+        player.facing_right = False
+    else:
+        player.facing_right = True
+
+
+
+#---------------- PIECES CREATION ----------------#
 def draw():
     screen.clear()
 
@@ -51,5 +157,8 @@ def draw():
 
     for tile in object_tiles:
         tile.draw()
+
+    player.draw()
+    cursor.draw()
 
 pgzrun.go()
